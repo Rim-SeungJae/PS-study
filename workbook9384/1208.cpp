61eb7210f86f5243c0dcc667ecc12cdd0f4301ec
dipreez <calloffate@naver.com>
2023-02-13 16:38:31 +0900

    23/02/13

--- workbook9384/1208.cpp
+++ workbook9384/1208.cpp
@@ -1,13 +1,37 @@
 #include<cstdio>
+#include<vector>
+#include<unordered_map>
 
-int cnt,n,s,arr[44];
+using namespace std;
+
+long long int cnt;
+int n,s,arr[44];
+unordered_map<int,int> mem;
 
 void bt(int sum,int c)
 {
-	if(sum == s&&c!=-1) cnt++;
-	for(int i=c+1;i<n;i++)
+	if(mem.find(sum)!=mem.end())
+	{
+		mem[sum] = mem[sum] + 1;
+	}
+	else{
+		mem[sum] = 1;
+	}
+	for(int i=c+1;i<=n/2;i++)
 	{
 		bt(sum + arr[i],i);
+	}
+}
+
+void btr(int sum,int c)
+{
+	if(mem.find(s-sum)!=mem.end())
+	{
+		cnt+=mem[s-sum];
+	}
+	for(int i=c+1;i<n;i++)
+	{
+		btr(sum + arr[i],i);
 	}
 }
 
@@ -19,5 +43,13 @@ int main()
 		scanf("%d",&arr[i]);
 	}
 	bt(0,-1);
-	printf("%d",cnt);
+	btr(0,n/2);
+	if(s==0) cnt--;
+	printf("%lld",cnt);
 }
+/*
+브루트포스
+배열을 반으로 나눠서 각각 따로 백트래킹을 진행 후 결과를 합치는 방식으로 풀어야  시간초과가 나지 않는다
+또, 가능한 경우의 수가 int의 범위를 벗어날 수도 있으므로 cnt 변수를 long long 타입으로 선언해야 한다.
+여러모로 신경쓸 것이 많은 까다로운 문제.
+*/ 
--- workbook9384/2143.cpp
+++ workbook9384/2143.cpp
@@ -1,8 +1,14 @@
 #include<cstdio>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
 
 int main()
 {
-	int a[1010]={0,},b[1010]={0,},t,n,m,sum=0,result=0;
+	int a[1010]={0,},b[1010]={0,},t,n,m,sum=0;
+	long long int result=0;
+	vector<int> a_sum,b_sum; 
 	scanf("%d",&t);
 	scanf("%d",&n);
 	for(int i=1;i<=n;i++)
@@ -22,24 +28,34 @@ int main()
 		for(int j=0;j<i;j++)
 		{
 			sum-=a[j];
-			for(int k=1;k<=m;k++)
-			{
-				sum+=b[k];
-				for(int l=0;l<k;l++)
-				{
-					sum-=b[l];
-					if(sum==t)
-					{
-						//printf("%d %d %d %d\n",i,j,k,l);
-						result++;
-					}
-					sum+=b[l];
-				}
-				sum-=b[k];
-			}
+			a_sum.push_back(sum);
 			sum+=a[j];
 		}
 		sum-=a[i];
 	}
-	printf("%d",result);
+	sum = 0;
+	for(int i=1;i<=m;i++)
+	{
+		sum+=b[i];
+		for(int j=0;j<i;j++)
+		{
+			sum-=b[j];
+			b_sum.push_back(sum);
+			sum+=b[j];
+		}
+		sum-=b[i];
+	}
+	sort(a_sum.begin(),a_sum.end());
+	sort(b_sum.begin(),b_sum.end());
+	for(int i=0;i<a_sum.size();i++)
+	{
+		int target = t-a_sum[i];
+		result += upper_bound(b_sum.begin(),b_sum.end(),target) - lower_bound(b_sum.begin(),b_sum.end(),target);
+	}
+	printf("%lld",result);
 }
+/*
+브루트포스 
+정렬 및 lower_bound, upper_bound 함수를 활용한 이진탐색으로 코드의 효율성을 높이는 것이 포인트.
+result 변수의 오버플로우에 주의하자
+*/ 
