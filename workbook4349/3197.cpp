e05dbbae798cefe473ec8bebaf11ab5bf14a8d73
dipreez <calloffate@naver.com>
2023-09-07 16:33:16 +0900

    23/09/07

--- workbook4349/3197.cpp
+++ workbook4349/3197.cpp
@@ -23,6 +23,7 @@ char arr[1550][1550];
 int w[1550][1550] = {0,},dx[4]={-1,0,1,0},dy[4]={0,-1,0,1};
 bool visited[1550][1550] = {0,};
 priority_queue<node> pq;
+queue<pair<int,int>> q;
 
 int main()
 {
@@ -50,25 +51,21 @@ int main()
 			}
 		}
 	}
-	int t=0;
-	while(cnt)
+	int t=1;
+	for(int i=0;i<r;i++)
 	{
-		t++;
-		for(int i=0;i<r;i++)
+		for(int j=0;j<c;j++)
 		{
-			for(int j=0;j<c;j++)
+			if(arr[i][j]=='X')
 			{
-				if(arr[i][j]=='X' && !visited[i][j])
+				for(int k=0;k<4;k++)
 				{
-					for(int k=0;k<4;k++)
+					if(i+dx[k]>=0 && i+dx[k]<r && j+dy[k]>=0 && j+dy[k]<c && arr[i+dx[k]][j+dy[k]] == '.' || arr[i+dx[k]][j+dy[k]] == 'L')
 					{
-						if(i+dx[k]>=0 && i+dx[k]<r && j+dy[k]>=0 && j+dy[k]<c && visited[i+dx[k]][j+dy[k]] && w[i+dx[k]][j+dy[k]]==t-1)
-						{
-							visited[i][j] = true;
-							w[i][j] = t;
-							cnt--;
-							break;
-						}
+						visited[i][j] = true;
+						w[i][j] = t;
+						q.push(make_pair(i,j));
+						break;
 					}
 				}
 			}
@@ -84,7 +81,38 @@ int main()
 		printf("\n");
 	}
 	*/
-	fill(&visited[0][0],&visited[r-1][c],0);
+	while(!q.empty())
+	{
+		t++;
+		int n = q.size();
+		for(int k=0;k<n;k++)
+		{
+			int tr = q.front().first, tc = q.front().second;
+			q.pop();
+			for(int i=0;i<4;i++)
+			{
+				if(tr+dx[i]>=0 && tr+dx[i]<r && tc+dy[i]>=0 && tc+dy[i]<c && !visited[tr+dx[i]][tc+dy[i]])
+				{
+					visited[tr+dx[i]][tc+dy[i]] = true;
+					w[tr+dx[i]][tc+dy[i]] = t;
+					q.push(make_pair(tr+dx[i],tc+dy[i]));
+				}
+			}
+		}
+		/*
+		printf("\n----------------------------------------------------------------------\n");
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+			{
+				printf("%d",w[i][j]);
+			}
+			printf("\n");
+		}
+		*/
+	}
+	
+	fill(&visited[0][0],&visited[1549][1550],0);
 	pq.push(node(L1_r,L1_c,0));
 	visited[L1_r][L1_c] = true;
 	int max_t = 0;
@@ -93,7 +121,6 @@ int main()
 		node target = pq.top();
 		pq.pop();
 		int tr = target.r, tc = target.c;
-		//printf("%d %d\n",tr,tc);
 		if(tr == L2_r && tc == L2_c)
 		{
 			break;
