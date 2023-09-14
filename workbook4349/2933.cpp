04556f4a6c23d2fbcc8c7bc460c67e190e1f7203
dipreez <calloffate@naver.com>
2023-09-14 15:12:04 +0900

    23/09/14

--- workbook4349/2933.cpp
+++ workbook4349/2933.cpp
@@ -1,9 +1,59 @@
 #include<cstdio>
+#include<algorithm>
 using namespace std;
 
-int dx[4] = {0,-1,0,1}, dy[4] = {1,0,-1,0},thr[2] = {1,-1};
+int dx[4] = {0,-1,0,1}, dy[4] = {1,0,-1,0},thr[2] = {1,-1},low[110] = {0,};
 int r,c,n,dir;
-char arr[110][110];
+char arr[110][110],tmp_arr[110][110];
+bool visited[110][110], isStable;
+
+void dfs(int x,int y)
+{
+	if(x<0 || y<0 || x>=r || y>=c) return;
+	if(x > low[y]) low[y] = x;
+	if(low[y] == r-1) isStable = true;
+	for(int i=0;i<4;i++)
+	{
+		if(!visited[x+dx[i]][y+dy[i]] && arr[x+dx[i]][y+dy[i]] == 'x')
+		{
+			visited[x+dx[i]][y+dy[i]] = true;
+			dfs(x+dx[i],y+dy[i]);
+		}
+	}
+	return;
+}
+
+void tmp_fall(int x,int y,int gap)
+{
+	if(x<0 || y<0 || x>=r || y>=c) return;
+	tmp_arr[x+gap][y] = 'x';
+	arr[x][y] = '.';
+	for(int i=0;i<4;i++)
+	{
+		if(!visited[x+dx[i]][y+dy[i]] && arr[x+dx[i]][y+dy[i]] == 'x')
+		{
+			visited[x+dx[i]][y+dy[i]] = true;
+			tmp_fall(x+dx[i],y+dy[i],gap);
+		}
+	}
+	return;
+}
+
+void fall(int x,int y,int gap)
+{
+	fill(&tmp_arr[0][0],&tmp_arr[109][110],'.');
+	tmp_fall(x,y,gap);
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)
+		{
+			if(tmp_arr[i][j] == 'x')
+			{
+				arr[i][j] = 'x';
+			}
+		}
+	}
+}
 
 int main()
 {
@@ -26,12 +76,45 @@ int main()
 		{
 			if(arr[r-h][idx] == 'x') break;
 		}
-		printf("%d\n",idx);
 		arr[r-h][idx] = '.';
 		dir ^= 1;
-		for(int j=0;j<3;j++)
+		fill(&visited[0][0],&visited[109][110],0);
+		for(int j=0;j<4;j++)
 		{
-			
+			if(r-h+dx[j]<0 || r-h+dx[j]>=r || idx+dy[j]<0 || idx+dy[j]>=c) continue;
+			if(arr[r-h+dx[j]][idx+dy[j]] == '.') continue;
+			if(visited[r-h+dx[j]][idx+dy[j]]) continue;
+			visited[r-h+dx[j]][idx+dy[j]] = true;
+			fill(&low[0],&low[110],-1);
+			isStable = false;
+			dfs(r-h+dx[j],idx+dy[j]);
+			/*
+			printf("%d %d:",r-h+dx[j],idx+dy[j]);
+			for(int k=0;k<c;k++)
+			{
+				printf("%d ",low[k]);
+			}
+			printf("\n");
+			*/
+			if(!isStable)
+			{
+				int gap = 110;
+				for(int k=0;k<c;k++)
+				{
+					if(low[k]>=0)
+					{
+						int l;
+						for(l=1;low[k]+l<r;l++)
+						{
+							if(arr[low[k]+l][k]=='x') break;
+						}
+						if(l-1<gap) gap = l-1;
+					}
+				}
+				fill(&visited[0][0],&visited[109][110],0);
+				fall(r-h+dx[j],idx+dy[j],gap);
+				break;
+			}
 		}
 	}
 	for(int i=0;i<r;i++)
@@ -42,5 +125,7 @@ int main()
 		}
 		printf("\n");
 	}
-
 }
+/*
+구현 시뮬레이션
+*/ 
