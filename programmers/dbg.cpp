dca3617060a07ccc6e01ab864b277b2647ddb91d
dipreez <calloffate@naver.com>
2024-03-21 17:57:36 +0900

    "24/03/21"

---
+++ programmers/dbg.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<vector<int>> dice) {
+    vector<int> answer;
+    int arr[1<<10][]
+    return answer;
+}
--- programmers/코딩테스트공부.cpp
+++ programmers/코딩테스트공부.cpp
@@ -6,35 +6,48 @@ using namespace std;
 
 int solution(int alp, int cop, vector<vector<int>> problems) {
     int answer = 2000000000;
-    int alp_req_max = 0, cop_req_max = 0;
+    int alp_req_max = alp, cop_req_max = cop;
     for(int i=0;i<problems.size();i++)
     {
         if(alp_req_max < problems[i][0]) alp_req_max = problems[i][0];
         if(cop_req_max < problems[i][1]) cop_req_max = problems[i][1];
     }
-    int dp[400][400];
-    fill(&dp[0][0],&dp[399][400],2000000000);
+    int dp[200][200];
+    fill(&dp[0][0],&dp[199][200],2000000000);
     dp[alp][cop] = 0;
-    for(int i=alp;i<=300;i++)
+    for(int i=alp;i<=alp_req_max;i++)
     {
-        for(int j=cop;j<=300;j++)
+        for(int j=cop;j<=cop_req_max;j++)
         {
-            if(i==alp&&j==cop) continue;
-            if(i>alp && dp[i][j] > dp[i-1][j] + 1) dp[i][j] = dp[i-1][j] + 1;
-            if(j>cop && dp[i][j] > dp[i][j-1] + 1) dp[i][j] = dp[i][j-1] + 1;
+            int x,y;
+            x = min(alp_req_max,i+1);
+            if(dp[x][j] > dp[i][j] + 1)
+            {
+                dp[x][j] = dp[i][j] + 1;
+            }
+            y = min(cop_req_max,j+1);
+            if(dp[i][y] > dp[i][j] + 1)
+            {
+                dp[i][y] = dp[i][j] + 1;
+            }
             for(int k = 0;k<problems.size();k++)
             {
                 int alp_req = problems[k][0],cop_req = problems[k][1],alp_rwd = problems[k][2],cop_rwd = problems[k][3],cost = problems[k][4];
-                if(i - alp_rwd >= alp_req && j - cop_rwd >= cop_req && dp[i][j] > dp[i-alp_rwd][j-cop_rwd] + cost) dp[i][j] = dp[i-alp_rwd][j-cop_rwd] + cost;
+                x = min(alp_req_max,i + alp_rwd);
+                y = min(cop_req_max,j + cop_rwd);
+                if(dp[x][y] > dp[i][j] + cost && i>=alp_req && j>=cop_req)
+                {
+                    dp[x][y] = dp[i][j] + cost;
+                }
             }
         }
     }
-    for(int i=alp_req_max;i<=300;i++)
-    {
-        for(int j=cop_req_max;j<=300;j++)
-        {
-            if(answer > dp[i][j]) answer = dp[i][j];
-        }
-    }
+    answer = dp[alp_req_max][cop_req_max];
     return answer;
+}
+
+int main()
+{
+    vector<vector<int>> problems({{10,15,2,1,2},{20,20,3,3,4}});
+    printf("%d ",solution(10,10,problems));
 }
